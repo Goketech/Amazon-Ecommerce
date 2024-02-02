3583f8220ea80273a16add8c452a78e0546f53bb
Modupe Akanni <modupe775@gmail.com>
2024-02-02 11:54:03 +0100

    add Navbar

--- frontend/ecommerce/src/App.jsx
+++ frontend/ecommerce/src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import NavBar from './components/NavBar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Router>
+      <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
